894310cc428795b88fcd14efe05e7d08aa70eafc
Harika-Devarashetty <devarashettyharika741@gmail.com>
2022-01-18 16:51:04 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular template driven form';
+ loginUser(item:any)
+ {
+console.warn(item)
+ }
+}
